cc2e02251113d00834e1a7076e0c8185867fbfdf
何承翰 <hokaso@qq.com>
2020-02-09 23:07:29 +0800

    base_settings

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  router,
+  store,
   render: h => h(App),
-}).$mount('#app')
+})
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+export const constantRoutes = [
+    {
+        path: '/redirect',
+        component: Layout,
+        hidden: true,
+        children: [
+            {
+                path: '/redirect/:path*',
+                component: () => import('@/views/redirect/index')
+            }
+        ]
+    },
+]
+
+const createRouter = () => new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+    const newRouter = createRouter()
+    router.matcher = newRouter.matcher // reset router
+}
+
+export default router
